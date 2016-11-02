2e4ee71834d3823c975db43767d6b8f1a9419ba4
pooya parsa dadashi <pooya_parsa_dadashi@yahoo.com>
2016-11-02 05:18:09 +0330

    Create fa-IR.php
    for iranian (farsi)

---
+++ translations/fa-IR.php
@@ -0,0 +1,19 @@
+<?php
+
+// For Iranian (Farsi | Persian)
+// By Pooya Parsa Dadashi
+
+$timeAgoStrings = array(
+  'aboutOneDay' => "1 روز قبل",
+  'aboutOneHour' => "حدود 1 ساعت قبل",
+  'aboutOneMonth' => "حدود 1 ماه قبل",
+  'aboutOneYear' => "حدود 1 سال قبل",
+  'days' => "%sروز قبل",
+  'hours' => "%sساعت قبل",
+  'lessThanAMinute' => "کمتر از 1 دقیقه قبل",
+  'lessThanOneHour' => "%sدقیقه قبل",
+  'months' => "%sماه قبل",
+  'oneMinute' => "1 دقیقه قبل",
+  'years' => "بیش از %s سال قبل",
+  'never' => 'هرگز'
+);
